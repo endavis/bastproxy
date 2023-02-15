b0a7553f75ab0c5c1f0fdbfa74211a9c1dffeb34
Eric Davis <endavis@gmail.com>
2023-02-15 17:34:23 +0000

    update for header and importing telnetlib3

--- libs/net/server.py
+++ libs/net/server.py
@@ -1,4 +1,22 @@
-import telnetlib3
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: libs/net/server.py
+#
+# File Description: creates the server for the proxy
+#
+# By: Bast
+# Standard Library
+import sys
+
+# 3rd Party
+try:
+    import telnetlib3
+except ImportError:
+    print('Please install required libraries. telnetlib3 is missing.')
+    print('From the root of the project: pip(3) install -r requirements.txt')
+    sys.exit(1)
+
+# Project
 
 class CustomTelnetServer(telnetlib3.TelnetServer):
 
