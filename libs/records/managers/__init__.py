55b80a28f818093b4b46a108eed46649d48d2221
Eric Davis <endavis@gmail.com>
2023-02-16 06:29:11 +0000

    preparing to refactor the record module

---
+++ libs/records/managers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: libs/records/managers/__init__.py
+#
+# File Description: holds the managers for the records library
+#
+# By: Bast
+"""
+Holds the managers for the records library
+"""
---
+++ libs/records/rtypes/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: libs/records/rtypes/__init__.py
+#
+# File Description: Holds the record types for the records library
+#
+# By: Bast
+"""
+Holds the record types for the records library
+"""
