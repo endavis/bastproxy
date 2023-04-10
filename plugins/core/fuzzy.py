e621b55b8168ddc621a2d371d9bd0f81316d6f5a
Eric Davis <endavis@gmail.com>
2023-04-10 10:45:28 +0100

    refactor fuzzy
    use rapidfuzz
    add ability to specify the scoring function
    add get_top_matches api

--- plugins/core/fuzzy.py
+++ plugins/core/fuzzy.py
@@ -13,7 +13,7 @@ import sys
 
 # 3rd Party
 try:
-    from fuzzywuzzy import process
+    import rapidfuzz
 except ImportError:
     print('Please install required libraries. fuzzywuzzy is missing.')
     print('From the root of the project: pip(3) install -r requirements.txt')
@@ -36,7 +36,7 @@ def sort_fuzzy_result(result):
     """
     newdict = {}
     for i in result:
-        if not i[1] in newdict:
+        if i[1] not in newdict:
             newdict[i[1]] = []
         newdict[i[1]].append(i[0])
     return newdict
@@ -52,36 +52,81 @@ class Plugin(BasePlugin):
         super().__init__(*args, **kwargs)
 
         self.api('libs.api:add')('get:best:match', self.get_best_match)
+        self.api('libs.api:add')('get:top:matches', self.get_top_matches)
 
         self.dependencies = []
         #self.dependencies = ['core.errors', 'core.msg']
 
     # get the best match for a string in a list of strings
-    def get_best_match(self, item_to_match, list_to_match):
+    def get_best_match(self, item_to_match, list_to_match, score_cutoff = 80,
+                       scorer: str = 'ratio') -> str:
         """  get the best match for a string in a list of strings
         @Yitem_to_match@w  = the string to find the closest match for
         @Ylist_to_match@w  = the list of strings to match against
 
+        scorer can be ratio, partial_ratio, token_sort_ratio,
+                token_set_ratio, partial_token_sort_ratio,
+                partial_token_set_ratio
+
         this function returns:
             a string of the item that best matched or None"""
-        found = None
-        LogRecord(f"get_best_match - item_to_match: {item_to_match}",
+        found = ''
+        scorer_inst = rapidfuzz.fuzz.__dict__[scorer]
+        LogRecord(f"get_best_match - {item_to_match=}, {scorer=} {score_cutoff=}",
                   level='debug', sources=[self.plugin_id]).send()
-        LogRecord(f"get_best_match - list_to_match: {list_to_match}",
+        LogRecord(f"get_best_match - {list_to_match=}",
                   level='debug', sources=[self.plugin_id]).send()
-        matching_startswith = [i for i in list_to_match if i.startswith(item_to_match)]
-        if len(matching_startswith) == 1:
-            found = matching_startswith[0]
-            LogRecord(f"get_best_match (startswith) matched {item_to_match} to {found}",
+        if item_to_match in list_to_match:
+            found = item_to_match
+            LogRecord(f"get_best_match (exact) matched {item_to_match} to {found}",
                       level='debug', sources=[self.plugin_id]).send()
         else:
-            sorted_extract = sort_fuzzy_result(process.extract(item_to_match, list_to_match))
-            LogRecord(f"get_best_match - extract for {item_to_match} - {sorted_extract}",
-                       level='debug', sources=[self.plugin_id]).send()
-            maxscore = max(sorted_extract.keys())
-            if maxscore > 80 and len(sorted_extract[maxscore]) == 1:
-                found = sorted_extract[maxscore][0]
-                LogRecord(f"get_best_match - (score) matched {item_to_match} to {found}",
-                            level='debug', sources=[self.plugin_id])
+            matching_startswith = [i for i in list_to_match if i.startswith(item_to_match)]
+            if len(matching_startswith) == 1:
+                found = matching_startswith[0]
+                LogRecord(f"get_best_match (startswith) matched {item_to_match} to {found}",
+                        level='debug', sources=[self.plugin_id]).send()
+            else:
+                sorted_extract = sort_fuzzy_result(rapidfuzz.process.extract(item_to_match, list_to_match, scorer=scorer_inst))
+                LogRecord(f"get_best_match - extract for {item_to_match} - {sorted_extract}",
+                        level='debug', sources=[self.plugin_id]).send()
+                maxscore = max(sorted_extract.keys())
+                if maxscore > score_cutoff and len(sorted_extract[maxscore]) == 1:
+                    found = sorted_extract[maxscore][0]
+                    LogRecord(f"get_best_match - (score) matched {item_to_match} to {found}",
+                                level='debug', sources=[self.plugin_id])
 
+        return found
+
+    def get_top_matches(self, item_to_match, list_to_match, items=5, score_cutoff=80, scorer: str = 'ratio') -> list:
+        """
+        scorer can be ratio, partial_ratio, token_sort_ratio,
+                        token_set_ratio, partial_token_sort_ratio,
+                        partial_token_set_ratio
+
+        get the top fuzzy matches for a string
+        """
+        scorer_inst = rapidfuzz.fuzz.__dict__[scorer]
+        found = []
+        print(f"get_top_matches - {item_to_match =} {score_cutoff =}")
+        LogRecord(f"get_top_matches - {item_to_match =} {items =} {score_cutoff =}",
+                  level='debug', sources=[self.plugin_id]).send()
+        LogRecord(f"get_top_matches - list_to_match: {list_to_match}",
+                  level='debug', sources=[self.plugin_id]).send()
+
+        extract = rapidfuzz.process.extract(item_to_match, list_to_match, scorer=scorer_inst, limit=items)
+        sorted_extract = sort_fuzzy_result(extract)
+        pprint.pprint(f"{extract =}")
+        LogRecord(f"get_best_match - extract for {item_to_match} - {sorted_extract}",
+                    level='debug', sources=[self.plugin_id]).send()
+        for i in sorted(sorted_extract.keys(), reverse=True):
+            if i > score_cutoff:
+                found.extend(sorted_extract[i])
+            if len(found) >= items:
+                break
+
+        LogRecord(f"get_top_matches - (score) matched {item_to_match} to {found}",
+                    level='debug', sources=[self.plugin_id])
+
+        print(f"get_top_matches - (score) matched {item_to_match} to {found}")
         return found
