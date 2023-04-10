715b6baec1d6ca02a55412f9d7fa92e9ecabb7e7
Eric Davis <endavis@gmail.com>
2023-04-10 21:49:06 +0100

    fix api call

--- plugins/core/proxy.py
+++ plugins/core/proxy.py
@@ -384,7 +384,7 @@ class Plugin(BasePlugin):
         """
         a function to restart the proxy after a timer
         """
-        self.api('plugins.core.pluginm:save:state')()
+        self.api('plugins.core.pluginm:save:all:plugins:state')()
 
         self.api(f"{self.plugin_id}:proxy:shutdown")()
 
