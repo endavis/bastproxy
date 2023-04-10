f62d72cdce28e2458cdac97f86b36fba72a2ece1
Eric Davis <endavis@gmail.com>
2023-04-10 17:25:21 +0100

    only use the top_level logger for toggling purposes

--- plugins/core/log.py
+++ plugins/core/log.py
@@ -32,6 +32,12 @@ AUTHOR = 'Bast'
 VERSION = 1
 REQUIRED = True
 
+def get_toplevel(logger_name):
+    """
+    get the toplevel logger from a name
+    """
+    return logger_name.split(':')[0] if ":" in logger_name else logger_name
+
 class Plugin(BasePlugin):
     """
     a class to manage logging
@@ -87,17 +93,18 @@ class Plugin(BasePlugin):
         """
         add a log count
         """
-        if logtype not in self.type_counts:
-            self.type_counts[logtype] = {
+        logger_name = get_toplevel(logtype)
+        if logger_name not in self.type_counts:
+            self.type_counts[logger_name] = {
                 'debug': 0,
                 'info': 0,
                 'warning': 0,
                 'error': 0,
                 'critical': 0,
             }
-        if level not in self.type_counts[logtype]:
-            self.type_counts[logtype][level] = 0
-        self.type_counts[logtype][level] += 1
+        if level not in self.type_counts[logger_name]:
+            self.type_counts[logger_name][level] = 0
+        self.type_counts[logger_name][level] += 1
 
     def _api_get_level_color(self, level):
         """
@@ -125,11 +132,12 @@ class Plugin(BasePlugin):
 
         if the logger hasn't been seen, it will default to logging.INFO
         """
-        if logger not in self.handlers['console']:
-            self.handlers['console'][logger] = 'info'
+        logger_name = get_toplevel(logger)
+        if logger_name not in self.handlers['console']:
+            self.handlers['console'][logger_name] = 'info'
             self.handlers['console'].sync()
 
-        convlevel = getattr(logging, self.handlers['console'][logger].upper(), None)
+        convlevel = getattr(logging, self.handlers['console'][logger_name].upper(), logging.INFO)
         return level >= convlevel
 
     def _api_can_log_to_file(self, logger, level):
@@ -138,11 +146,12 @@ class Plugin(BasePlugin):
 
         if the logger hasn't been seen, it will default to logging.INFO
         """
-        if logger not in self.handlers['file']:
-            self.handlers['file'][logger] = 'info'
+        logger_name = get_toplevel(logger)
+        if logger_name not in self.handlers['file']:
+            self.handlers['file'][logger_name] = 'info'
             self.handlers['file'].sync()
 
-        convlevel = getattr(logging, self.handlers['file'][logger].upper(), None)
+        convlevel = getattr(logging, self.handlers['file'][logger_name].upper(), logging.INFO)
         return level >= convlevel
 
     def _api_can_log_to_client(self, logger, level):
@@ -152,12 +161,9 @@ class Plugin(BasePlugin):
         if the logger hasn't been seen, do not allow logging to the client
         logging to the client must be explicitly enabled
         """
-        if logger in self.handlers['client']:
-            convlevel = getattr(logging, self.handlers['client'][logger].upper(), None)
-            if level >= convlevel:
-                return True
-
-        return False
+        logger_name = get_toplevel(logger)
+        return logger_name in self.handlers['client'] and level >= getattr(
+            logging, self.handlers['client'][logger_name].upper(), logging.INFO)
 
     # toggle logging a logtype to the clients
     def api_set_log_to_client(self, logtype, level: str='info', flag=True):
@@ -166,21 +172,25 @@ class Plugin(BasePlugin):
         @Yflag@w      = True to send to clients, false otherwise (default: True)
 
         this function returns no values"""
+        LogRecord(f"api_set_log_to_client: {logtype} {level} {flag}",
+                    level='debug', sources=[self.plugin_id]).send()
+
+        logger_name = get_toplevel(logtype)
         if isinstance(level, numbers.Number):
             level = logging.getLevelName(level).lower()
         if not level:
             LogRecord(f"api_toggletoclient: invalid log level {level}",
-                      level='error', sources=[self.plugin_id, logtype]).send()
+                      level='error', sources=[self.plugin_id, logger_name]).send()
             return
 
-        if flag and logtype not in self.handlers['client']:
-            self.handlers['client'][logtype] = level
-            LogRecord(f"setting {logtype} to log to client at level {level}",
-                      level='debug', sources=[self.plugin_id, logtype]).send()
-        elif not flag and logtype in self.handlers['client']:
-            del(self.handlers['client'][logtype])
-            LogRecord(f"removing {logtype} logging to client {level}",
-                      level='debug', sources=[self.plugin_id, logtype]).send()
+        if flag and logger_name not in self.handlers['client']:
+            self.handlers['client'][logger_name] = level
+            LogRecord(f"setting {logger_name} to log to client at level {level}",
+                      level='debug', sources=[self.plugin_id, logger_name]).send()
+        elif not flag and logger_name in self.handlers['client']:
+            del(self.handlers['client'][logger_name])
+            LogRecord(f"removing {logger_name} logging to client {level}",
+                      level='debug', sources=[self.plugin_id, logger_name]).send()
 
         self.handlers['client'].sync()
 
@@ -194,11 +204,12 @@ class Plugin(BasePlugin):
         if args['logtype']:
             remove = not args['remove']
             for i in args['logtype']:
-                self.api(f"{self.plugin_id}:set:log:to:client")(i, level=level, flag=remove)
-                if i in self.handlers['client']:
-                    tmsg.append(f"setting {i} to log to client at level {level}")
+                logger_name = get_toplevel(i)
+                self.api(f"{self.plugin_id}:set:log:to:client")(logger_name, level=level, flag=remove)
+                if logger_name in self.handlers['client']:
+                    tmsg.append(f"setting {logger_name} to log to client at level {level}")
                 else:
-                    tmsg.append(f"no longer sending {i} to client")
+                    tmsg.append(f"no longer sending {logger_name} to client")
 
             self.handlers['client'].sync()
             return True, tmsg
@@ -223,16 +234,20 @@ class Plugin(BasePlugin):
         @Yflag@w      = True to send to console, false otherwise (default: True)
 
         this function returns no values"""
+        LogRecord(f"api_set_log_to_console: {logtype} {level}",
+                    level='debug', sources=[self.plugin_id]).send()
+
+        logger_name = get_toplevel(logtype)
         if isinstance(level, numbers.Number):
             level = logging.getLevelName(level).lower()
         if not level:
             LogRecord(f"api_set_log_to_console: invalid log level {level}",
-                      level='error', sources=[self.plugin_id, logtype]).send()
+                      level='error', sources=[self.plugin_id, logger_name]).send()
             return
 
-        self.handlers['console'][logtype] = level
-        LogRecord(f"setting {logtype} to log to console at level {level}",
-                  level='debug', sources=[self.plugin_id, logtype]).send()
+        self.handlers['console'][logger_name] = level
+        LogRecord(f"setting {logger_name} to log to console at level {level}",
+                  level='debug', sources=[self.plugin_id, logger_name]).send()
 
         self.handlers['console'].sync()
 
@@ -244,14 +259,15 @@ class Plugin(BasePlugin):
         tmsg = []
         if args['logtype']:
             for i in args['logtype']:
-                if i not in self.handlers['console']:
-                    self.handlers['console'][i] = 'info'
-                if self.handlers['console'][i] == 'info':
-                    self.api(f"{self.plugin_id}:set:log:to:console")(i, level='debug')
-                    tmsg.append(f"setting {i} to log to console at level 'debug'")
+                logger_name = get_toplevel(i)
+                if logger_name not in self.handlers['console']:
+                    self.handlers['console'][logger_name] = 'info'
+                if self.handlers['console'][logger_name] == 'info':
+                    self.api(f"{self.plugin_id}:set:log:to:console")(logger_name, level='debug')
+                    tmsg.append(f"setting {logger_name} to log to console at level 'debug'")
                 else:
-                    self.api(f"{self.plugin_id}:set:log:to:console")(i, level='info')
-                    tmsg.append(f"setting {i} to log to console at default level 'info'")
+                    self.api(f"{self.plugin_id}:set:log:to:console")(logger_name, level='info')
+                    tmsg.append(f"setting {logger_name} to log to console at default level 'info'")
 
             self.handlers['console'].sync()
             return True, tmsg
@@ -283,12 +299,12 @@ class Plugin(BasePlugin):
                       level='error', sources=[self.plugin_id, logtype]).send()
             return
 
-        self.handlers['file'][logtype] = level
-        LogRecord(f"setting {logtype} to log to file at level {level}",
-                  level='debug', sources=[self.plugin_id, logtype]).send()
+        logger_name = get_toplevel(logtype)
+        self.handlers['file'][logger_name] = level
+        LogRecord(f"setting {logger_name} to log to file at level {level}",
+                  level='debug', sources=[self.plugin_id, logger_name]).send()
 
         self.handlers['file'].sync()
-
 
     # toggle a logtype to log to a file
     def cmd_file(self, args):
@@ -298,14 +314,15 @@ class Plugin(BasePlugin):
         tmsg = []
         if args['logtype']:
             for i in args['logtype']:
-                if i not in self.handlers['file']:
-                    self.handlers['file'][i] = 'info'
-                if self.handlers['file'][i] == 'info':
-                    self.api(f"{self.plugin_id}:set:log:to:file")(i, level='debug')
-                    tmsg.append(f"setting {i} to log to file at level 'debug'")
+                logger_name = get_toplevel(i)
+                if logger_name not in self.handlers['file']:
+                    self.handlers['file'][logger_name] = 'info'
+                if self.handlers['file'][logger_name] == 'info':
+                    self.api(f"{self.plugin_id}:set:log:to:file")(logger_name, level='debug')
+                    tmsg.append(f"setting {logger_name} to log to file at level 'debug'")
                 else:
-                    self.api(f"{self.plugin_id}:set:log:to:file")(i, level='info')
-                    tmsg.append(f"setting {i} to log to file at default level 'info'")
+                    self.api(f"{self.plugin_id}:set:log:to:file")(logger_name, level='info')
+                    tmsg.append(f"setting {logger_name} to log to file at default level 'info'")
 
             self.handlers['file'].sync()
             return True, tmsg
