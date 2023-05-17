de462099197fed67e909c2328ebb73539bd44a1b
Eric Davis <endavis@gmail.com>
2023-05-17 17:52:24 +0100

    reindent everything with 4 spaces

--- libs/timing.py
+++ libs/timing.py
@@ -23,69 +23,69 @@ API = BASEAPI(owner_id=__name__)
 
 
 def duration(func):
-  """
-  a decorator to find the duration of a function
-  """
-  @wraps(func)
-  def wrapper(*arg):
     """
-    the wrapper to find the duration of a function
+    a decorator to find the duration of a function
     """
-    uid = uuid4().hex
-    tname = f"{func.func_name}"
-    API('libs.timing:start')(uid, tname, arg)
-    res = func(*arg)
-    API('libs.timing:finish')(uid, tname, arg)
-    return res
-  return wrapper
+    @wraps(func)
+    def wrapper(*arg):
+        """
+        the wrapper to find the duration of a function
+        """
+        uid = uuid4().hex
+        tname = f"{func.func_name}"
+        API('libs.timing:start')(uid, tname, arg)
+        res = func(*arg)
+        API('libs.timing:finish')(uid, tname, arg)
+        return res
+    return wrapper
 
 class Timing(object):
-  """
-  manage timing functions
-  """
-  def __init__(self):
     """
-    create the dictionary
+    manage timing functions
     """
-    self.api = API
-    self.enabled = True
+    def __init__(self):
+        """
+        create the dictionary
+        """
+        self.api = API
+        self.enabled = True
 
-    self.timing = {}
+        self.timing = {}
 
-    self.api('libs.api:add')('libs.timing', 'start', self._api_start)
-    self.api('libs.api:add')('libs.timing', 'finish', self._api_finish)
-    self.api('libs.api:add')('libs.timing', 'toggle', self._api_toggle)
+        self.api('libs.api:add')('libs.timing', 'start', self._api_start)
+        self.api('libs.api:add')('libs.timing', 'finish', self._api_finish)
+        self.api('libs.api:add')('libs.timing', 'toggle', self._api_toggle)
 
-  def _api_toggle(self, tbool=None):
-    """
-    toggle the timing flag
-    """
-    self.enabled = not self.enabled if tbool is None else bool(tbool)
+    def _api_toggle(self, tbool=None):
+        """
+        toggle the timing flag
+        """
+        self.enabled = not self.enabled if tbool is None else bool(tbool)
 
-  def _api_start(self, uid, timername, args=None):
-    """
-    start a timer
-    """
-    if self.enabled:
-      owner_id = self.api('libs.api:get.caller.owner')()
-      self.timing[uid] = {'name': timername, 'start': default_timer(),
-                          'owner_id': owner_id}
-      LogRecord(f"starttimer - {uid} {timername:<20} : started - from {owner_id} with args {args}",
-                level='debug', sources=[__name__, owner_id])()
+    def _api_start(self, uid, timername, args=None):
+        """
+        start a timer
+        """
+        if self.enabled:
+            owner_id = self.api('libs.api:get.caller.owner')()
+            self.timing[uid] = {'name': timername, 'start': default_timer(),
+                                'owner_id': owner_id}
+            LogRecord(f"starttimer - {uid} {timername:<20} : started - from {owner_id} with args {args}",
+                      level='debug', sources=[__name__, owner_id])()
 
-  def _api_finish(self, uid, timername, args=None):
-    """
-    finish a timer
-    """
-    if self.enabled:
-      timerfinish = default_timer()
-      if uid in self.timing:
-        LogRecord(f"finishtimer - {uid} {timername:<20} : finished in {(timerfinish - self.timing[uid]['start']) * 1000.0} ms - with args {args}",
-                    level='debug', sources=[__name__, self.timing[uid]['owner_id']])()
-        del self.timing[timername]
-      else:
-        owner_id = self.api('libs.api:get.caller.owner')()
-        LogRecord(f"finishtimer - {uid} {timername:<20} : not found - called from {owner_id}",
-                    level='error', sources=[__name__, owner_id])()
+    def _api_finish(self, uid, timername, args=None):
+        """
+        finish a timer
+        """
+        if self.enabled:
+            timerfinish = default_timer()
+            if uid in self.timing:
+                LogRecord(f"finishtimer - {uid} {timername:<20} : finished in {(timerfinish - self.timing[uid]['start']) * 1000.0} ms - with args {args}",
+                            level='debug', sources=[__name__, self.timing[uid]['owner_id']])()
+                del self.timing[timername]
+            else:
+                owner_id = self.api('libs.api:get.caller.owner')()
+                LogRecord(f"finishtimer - {uid} {timername:<20} : not found - called from {owner_id}",
+                            level='error', sources=[__name__, owner_id])()
 
 TIMING = Timing()
