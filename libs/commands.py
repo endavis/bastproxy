c94a8c263d59e101235d36ae72b6042f358a39fc
Eric Davis <endavis@gmail.com>
2023-11-06 17:13:44 +0000

    update documenation on adding commands

--- libs/commands.py
+++ libs/commands.py
@@ -8,51 +8,7 @@
 """
 This plugin hold command utilities
 
-All 3 decorators are not required.
-if any one of the decorators are used, the command plugin will
-mark this for command creation and use defaults for any missing
-variables.
-
-To apply a command to a plugin method, use the following decorator:
-    from libs.commands import AddCommand, AddParser, AddArgument
-
-    # see the arguments for plugins.core.commands.Command.__init__ kwargs
-    # There are a couple of special keywords that can be used:
-    #   dynamic_name - will make the command plugin
-    #       format the given string with the input as func.__self__.__dict__
-    #       and use that for the command name
-    #   autoadd - if False, the command will not be adding automatically
-    #        It can be added later, see plugins._baseplugin.BasePlugin._add_commands
-    @AddCommand(show_in_history=False)
-    # passed directly to ArgumentParser
-    @AddParser(description='list functions in the API')
-    # passed directly to ArgumentParser.add_argument
-    @AddArgument('toplevel',
-                    help='the top level api to show (optional)',
-                    default='', nargs='?')
-    @AddArgument('-np',
-                    '--noplugin',
-                    help="use an API that is not from a plugin",
-                    action='store_true')
-    def _command_list(self):
-        \"""
-        @G%(name)s@w - @B%(cmdname)s@w
-        List functions in the api
-          @CUsage@w: list @Y<apiname>@w
-          @Yapiname@w = (optional) the toplevel api to show
-        \"""
-        args = self.api('plugins.core.commands:get.current.command.args')()
-        tmsg = []
-
-        api = self.api
-        if args['noplugin']:
-            api = API(owner_id=f"{self.plugin_id}:_command_list")
-
-        if apilist := api('libs.api:list')(args['toplevel']):
-            tmsg.extend(apilist)
-        else:
-            tmsg.append(f"{args['toplevel']} does not exist in the api")
-        return True, tmsg
+see info/add_commands.txt for more info
 """
 # Standard Library
 
