328e7122ed6bcafb5bfc93c35d036ebe0c9b8074
Eric Davis <6662995+endavis@users.noreply.github.com>
2024-10-29 13:42:51 +0000

    new classes for data
    NetworkDataLine for a single line of text
    NetworkData is a UserList of NetworkDataLines
    ToClientData is for handling data to send to a client
       will eventually replace ToClientRecord

---
+++ libs/records/rtypes/clientdata.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: libs/records/rtypes/toclient.py
+#
+# File Description: Holds the client record type
+#
+# By: Bast
+"""
+Holds the client record type
+"""
+# Standard Library
+
+# 3rd Party
+
+# Project
+from libs.records.rtypes.log import LogRecord
+from libs.records.rtypes.base import BaseListRecord, BaseRecord
+from libs.records.rtypes.networkdata import NetworkData
+
+SETUPEVENTS = False
+
+class ToClientData(BaseRecord):
+    """
+    a record to a client, this can originate with the mud or internally
+    data from the mud will immediately be transformed into this type of record
+    will not neccesarily end up going to the client
+
+    The message format is a list of NetworkDataLine objects
+    """
+    def __init__(self, message: BaseNetworkDataRecord,
+                 clients: list|None=None, exclude_clients: list|None=None, preamble=True,
+                 prelogin: bool=False, error: bool=False, color_for_all_lines=None):
+        """
+        initialize the class
+        """
+        super().__init__()
+        self.message = message
+        self.add_related_record(message)
+        # flag to include preamble when sending to client
+        self.preamble: bool = preamble
+        # flag to send to client before login
+        self.prelogin: bool = prelogin
+        # flag for this is an error message
+        self.error: bool = error
+        # This is so that events can set this and it will not be sent to the client
+        self.send_to_clients: bool = True
+        # clients to send to, a list of client uuids
+        # if this list is empty, it goes to all clients
+        self.clients: list[str] = clients or []
+        # clients to exclude, a list of client uuids
+        self.exclude_clients: list[str] = exclude_clients or []
+        # This will set the color for all lines to the specified @ color
+        self.color_for_all_lines: str = color_for_all_lines or ''
+        self.modify_data_event_name = 'ev_to_client_data_modify'
+        self.read_data_event_name = 'ev_to_client_data_read'
+
+        self.sending = False
+
+        self.setup_events()
+
+    def get_attributes_to_format(self):
+        attributes = super().get_attributes_to_format()
+        attributes[0].extend([('Preamble', 'preamble'), ('Prelogin', 'prelogin'),
+                              ('Error', 'error'), ('Send To Clients', 'send_to_clients'),
+                              ('Clients', 'clients'), ('Exclude Clients', 'exclude_clients'),
+                              ('Color For All Lines', 'color_for_all_lines')])
+        return attributes
+    
+    def setup_events(self):
+        global SETUPEVENTS
+        if not SETUPEVENTS:
+            SETUPEVENTS = True
+            self.api('plugins.core.events:add.event')(self.modify_data_event_name, __name__,
+                                                description=['An event to modify data before it is sent to the client'],
+                                                arg_descriptions={'line': 'The line to modify',
+                                                                  'sendtoclient': 'A flag to determine if this line should be sent to the client'})
+            self.api('plugins.core.events:add.event')(self.read_data_event_name, __name__,
+                                                description=['An event to see data that was sent to the client'],
+                                                arg_descriptions={'ToClientRecord': 'A libs.records.ToClientRecord object'})
+
+    @property
+    def noansi(self):
+        """
+        return the message without ansi codes
+        """
+        newmessage: list[str] = [
+            item.noansi for item in self.message
+        ]
+        return newmessage
+
+    @property
+    def color(self):
+        """
+        return the message with ansi codes converted to @ color codes
+        """
+        newmessage: list[str] = [
+            item.color for item in self.message
+        ]
+        return newmessage
+
+    def add_client(self, client_uuid: str):
+        """
+        add a client to the list of clients to send to
+        """
+        if client_uuid in self.exclude_clients:
+            self.exclude_clients.remove(client_uuid)
+        if client_uuid not in self.clients:
+            self.clients.append(client_uuid)
+
+    def exclude_client(self, client_uuid):
+        """
+        add a client to the list of clients to exclude
+        """
+        if client_uuid in self.clients:
+            self.clients.remove(client_uuid)
+        if client_uuid not in self.exclude_clients:
+            self.exclude_clients.append(client_uuid)
+
+    def can_send_to_client(self, client_uuid, internal):
+        """
+        returns true if this message can be sent to the client
+        """
+        if client_uuid:
+            # Exclude takes precedence over everything else
+            if client_uuid in self.exclude_clients:
+                return False
+            # If the client is a view client and this is an internal message, we don't send it
+            # This way view clients don't see the output of commands entered by other clients
+            if self.api('plugins.core.clients:client.is.view.client')(client_uuid) and internal:
+                return False
+            # If the client is in the list of clients or self.clients is empty,
+            # then we can check to make sure the client is logged in or the prelogin flag is set
+            if (not self.clients or client_uuid in self.clients) and (
+                self.api('plugins.core.clients:client.is.logged.in')(client_uuid)
+                or self.prelogin
+            ):
+                # All checks passed, we can send to this client
+                return True
+        return False
+
+    def _exec_(self, actor=''):
+        """
+        send the message
+        """
+        if self.sending:
+            LogRecord(f"LogRecord: {self.uuid} is already sending",
+                                level='debug', stack_info=True, sources=[__name__])()
+            return
+
+        self.sending = True
+        self.addupdate('Info', f"{'Start':<8}: send", actor)
+
+        for line in self.message:
+            # If it came from the mud and it is not a telnet command, 
+            # pass each line through the event system to allow plugins to modify it
+            if line.frommud and line.is_io:
+                event_args = self.api('plugins.core.events:raise.event')(self.modify_data_event_name, args={'line': line})
+
+                if event_args['line'].line_modified:
+                    self.addupdate('Modify', f"line modified by {self.modify_data_event_name}",
+                                    f"{actor}:send:{self.modify_data_event_name}",
+                                    extra={'line':line, 'event_args':event_args})
+                    self.add_related_record(event_args)
+
+                if not event_args['line'].send:
+                    self.addupdate('Modify', f"line removed because sendtoclient was set to False from {self.modify_data_event_name}",
+                                    f"{actor}:send:{self.modify_data_event_name}",
+                                    extra={'line':line})
+                    self.add_related_record(event_args)
+
+                self.addupdate('Info', f'After event {self.modify_data_event_name}', actor)
+
+                self.api('plugins.core.events:raise.event')(self.read_data_event_name, args={'line': line})
+
+            if self.send_to_clients and line.send:
+                line.format(preamble=self.preamble, color=self.color_for_all_lines)
+
+                clients = self.clients or self.api(
+                    'plugins.core.clients:get.all.clients'
+                )(uuid_only=True)
+                for client_uuid in clients:
+                    if self.can_send_to_client(client_uuid, line.internal):
+                        self.api('plugins.core.clients:send.to.client')(client_uuid, line)
+                    else:
+                        LogRecord(f"## NOTE: Client {client_uuid} cannot receive message {str(self.uuid)}",
+                                level='debug', sources=[__name__])()
+
+        self.addupdate('Info', f"{'Complete':<8}: send", f"{actor}:send")
+        self.sending = False
---
+++ libs/records/rtypes/networkdata.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: libs/records/rtypes/dataline.py
+#
+# File Description: Holds the records for network data
+#
+# By: Bast
+"""
+Holds the data line record
+"""
+# Standard Library
+from collections import UserList
+
+# 3rd Party
+
+# Project
+from libs.records.rtypes.base import BaseRecord
+from libs.records.rtypes.log import LogRecord
+
+class NetworkDataLine(BaseRecord):
+    """
+    A record to hold a line of data that will be sent to the clients
+    or the mud
+    """
+    def __init__(self, line, originated='internal', line_type='IO'):
+        BaseRecord.__init__(self, f"{self.__class__.__name__}:{line}")
+        self._attributes_to_monitor.append('line')
+        self._attributes_to_monitor.append('send')
+        if (isinstance(line, str) and ('\n' in line or '\r' in line)) or \
+           (isinstance(line, bytes) and (b'\n' in line or b'\r' in line)):
+            LogRecord(f"LogRecord: {self.uuid} {line} has is multi line with \\n and/or \\r",
+                                level='error', stack_info=True, sources=[__name__])()
+        self.line_type = line_type # IO, COMMAND-TELNET
+        self.originated = originated # mud, client, internal
+        if isinstance(line, bytes) and not self.is_command_telnet:
+            line = line.decode('utf-8')        
+        self.line: str | bytes = line
+        self.original_line = line
+        self.send = True
+        self.line_modified = False
+        self.is_prompt = False
+        if self.is_io:
+            self.noansi = self.api('plugins.core.colors:ansicode.strip')(line)
+            self.color = self.api('plugins.core.colors:ansicode.to.colorcode')(line)
+        else:
+            self.noansi = ''
+            self.color = ''
+
+    def _onchange_line(self, orig_value, new_value):
+        """
+        set the line_modified flag if the line changes
+        """
+        if orig_value != new_value:
+            self.line_modified = True
+
+    @property
+    def is_command_telnet(self):
+        """
+        A shortcut property to determine if this message is a Telnet Opcode.
+        """
+        return self.line_type == "COMMAND-TELNET"
+
+    @property
+    def is_io(self):
+        """
+        A shortcut property to determine if this message is normal I/O.
+        """
+        return self.line_type == "IO"
+    
+    @property
+    def internal(self):
+        """
+        A shortcut property to determine if this message is internal
+        """
+        return self.originated == 'internal'
+    
+    @property
+    def fromclient(self):
+        """
+        A shortcut property to determine if this message is from a client
+        """
+        return self.originated == 'client'
+    
+    @property
+    def frommud(self):
+        """
+        A shortcut property to determine if this message is from the mud
+        """
+        return self.originated == 'mud'
+
+    def add_line_endings(self, actor=''):
+        """
+        add line endings to the message
+        """
+        if self.is_io:
+            self.line = f"{self.line}\n\r"
+
+    def color_line(self, color: str, actor=''):
+        """
+        color the message and convert all colors to ansicodes
+
+        color is the color for all lines
+
+        actor is the item that ran the color function
+        """
+        if not self.is_io:
+            return
+        
+        if not self.api('libs.api:has')('plugins.core.colors:colorcode.to.ansicode'):
+            return
+        
+        if color:
+            if '@w' in self.line:
+                line_list = self.line.split('@w')
+                new_line_list = []
+                for item in line_list:
+                    if item:
+                        new_line_list.append(f"{color}{item}")
+                    else:
+                        new_line_list.append(item)
+                self.line = f"@w{color}".join(new_line_list)
+            if self.line:
+                self.line = f"{color}{self.line}@w"
+        self.line = self.api('plugins.core.colors:colorcode.to.ansicode')(self.line)
+
+    def format(self, preamble, color: str = ''):
+        """
+        format the message
+        """
+        if self.is_io:
+            if self.internal:
+                if preamble:
+                    self.add_preamble()
+                self.color_line(color)
+            self.add_line_endings()
+
+    def strip(self):
+        """
+        strip the line of carriage returns and line feeds
+        """
+        return self.line.strip()
+    
+    def get_attributes_to_format(self):
+        attributes = super().get_attributes_to_format()
+        for item in self._attributes_to_monitor:
+            orig_value = self._am_get_original_value(item)
+            if orig_value == getattr(self, item):
+                attributes[0].append((f"{item}", item))
+            else:
+                attributes[0].append((item, f"changed from '{self._am_get_original_value(item)}' to '{getattr(self, item)}'"))
+        attributes[0].append(('Line Modified', 'line_modified'))
+        attributes[0].append(('Originated', 'originated'))
+        attributes[0].append(('Type', 'line_type'))        
+        return attributes
+
+    def one_line_summary(self):
+        return repr(self.line)
+    
+    def __str__(self):
+        return self.line
+    
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.uuid} {self.originated} {self.original_line.strip()})'
+    
+    def add_preamble(self, error=False):
+        """
+        add the preamble to the line only if it is from internal and is an IO message
+        """
+        if self.internal and self.is_io:
+            preamblecolor = self.api('plugins.core.proxy:preamble.color.get')(error=error)
+            preambletext = self.api('plugins.core.proxy:preamble.get')()
+            self.line = f"{preamblecolor}{preambletext}@w: {self.line}"
+
+class NetworkData(UserList, BaseRecord):
+    """
+    this is a base record of a list of NetworkDataLine records
+    """
+    def __init__(self, message: NetworkDataLine | str| bytes | list[NetworkDataLine | str | bytes], owner_id: str='',
+                 track_record=True):
+        """
+        initialize the class
+        """
+        if not isinstance(message, list):
+            message = [message]
+
+        new_message = []
+        for item in message:
+            if not (isinstance(item, (NetworkDataLine, str, bytes))):
+                raise ValueError(f"item must be a NetworkDataLine object or a string, not {type(item)}")
+            if isinstance(item, (str, bytes)):
+                item = NetworkDataLine(item)
+            new_message.append(item)
+
+        UserList.__init__(self, new_message)
+        BaseRecord.__init__(self, owner_id, track_record=track_record)
+        for item in new_message:
+            self.add_related_record(item)        
+        self.owner_id = owner_id
+        # This is a flag to prevent the message from being sent  more than once
+        self.sending = False
+        self.addupdate('Modify', 'original input', extra={'list':f"{new_message}"})  
+
+    def one_line_summary(self):
+        """
+        get a one line summary of the record
+        """
+        first_str = ''
+        for networkdataitem in self:
+            if networkdataitem.line:
+                first_str = networkdataitem.original_line
+
+        return first_str or 'No data found'
+
+    def get_attributes_to_format(self):
+        attributes = super().get_attributes_to_format()
+        attributes[2].append(('Data', 'data'))
+
+        return attributes
+
+    def __setitem__(self, index, item: NetworkDataLine | str):
+        """
+        set the item
+        """
+        if not (isinstance(item, (NetworkDataLine, str, bytes))):
+            raise ValueError(f"item must be a NetworkDataLine object or a string, not {type(item)}")
+        if isinstance(item, (str, bytes)):
+            item = NetworkDataLine(item)
+        self.add_related_record(item)
+        super().__setitem__(index, item)
+        self.addupdate('Modify', f'set item at position {index}', extra={'item':f"{repr(item)}"})         
+
+    def insert(self, index, item: NetworkDataLine | str | bytes):
+        """
+        insert an item
+        """
+        if not (isinstance(item, (NetworkDataLine, str, bytes))):
+            raise ValueError(f"item must be a NetworkDataLine object or a string, not {type(item)}")
+        if isinstance(item, (str, bytes)):
+            item = NetworkDataLine(item)
+        self.add_related_record(item)
+        super().insert(index, item)
+        self.addupdate('Modify', f'inserted item into position {index}', extra={'item':f"{repr(item)}"})           
+
+    def append(self, item: NetworkDataLine | str | bytes):
+        """
+        append an item
+        """
+        if not (isinstance(item, (NetworkDataLine, str, bytes))):
+            raise ValueError(f"item must be a NetworkDataLine object or a string, not {type(item)}")
+        if isinstance(item, (str, bytes)):
+            item = NetworkDataLine(item)
+        self.add_related_record(item)
+        super().append(item)
+        self.addupdate('Modify', f'Appended item into position {len(self) - 1}', extra={'item':f"{repr(item)}"})            
+
+    def extend(self, items):
+        """
+        extend the list
+        """
+        new_list = []
+        for item in items:
+            if not (isinstance(item, (NetworkDataLine, str, bytes))):
+                raise ValueError(f"item must be a NetworkDataLine object or a string, not {type(item)}")
+            if isinstance(item, (str, bytes)):
+                item = NetworkDataLine(item)
+            self.add_related_record(item)
+            new_list.append(item)
+        super().extend(new_list)
+        self.addupdate('Modify', 'extended list', extra={'new_list':f"{new_list}"})         
