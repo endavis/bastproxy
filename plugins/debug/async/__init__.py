f9514c7c36a0c456fff27fa611df6e152a1e2ca2
Eric Davis <6662995+endavis@users.noreply.github.com>
2024-11-03 21:06:55 +0000

    add plugin to debug async internals
    list tasks for now

---
+++ plugins/debug/async/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: plugins/debug/async/_init_.py
+#
+# File Description: a plugin to inspect async internals
+#
+# By: Bast
+"""
+This plugin will allow you to inspect async internals
+"""
+
+# these 4 are required
+PLUGIN_NAME = 'Inspect async internals'
+PLUGIN_PURPOSE = 'see info about async tasks and events'
+PLUGIN_AUTHOR = 'Bast'
+PLUGIN_VERSION = 1
---
+++ plugins/debug/async/plugin/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Project: bastproxy
+# Filename: plugins/debug/records/plugin/_init_.py
+#
+# File Description: a plugin to inspect records
+#
+# By: Bast
+__all__ = ['Plugin']
+
+from ._async import AsyncPlugin as Plugin
