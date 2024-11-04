ab92e05550cc5278ff9f2eb07f584028e0b6d835
Eric Davis <6662995+endavis@users.noreply.github.com>
2024-11-04 11:56:39 +0000

    add attribute locking

--- libs/tracking/utils/attributes.py
+++ libs/tracking/utils/attributes.py
@@ -2,6 +2,7 @@ import contextlib
 class AttributeMonitor:
     def __init__(self):
         self._attributes_to_monitor = []
+        self._locked_attributes = []
         self._am_original_values = {}
 
     def __setattr__(self, name, value):
@@ -9,6 +10,9 @@ class AttributeMonitor:
             original_value = getattr(self, name)
         except AttributeError:
             original_value = '#!NotSet'
+        if hasattr(self, '_locked_attributes') and name in self._locked_attributes:
+            self._am_locked_attribute_update(name, value)
+            return
         super().__setattr__(name, value)
 
         if hasattr(self, '_attributes_to_monitor') and name in self._attributes_to_monitor:
@@ -28,4 +32,16 @@ class AttributeMonitor:
         return self._am_original_values.get(name, None)
 
     def _am_onchange__all(self, name, original_value, new_value):
+        pass
+
+    def _am_lock_attribute(self, name):
+        self._locked_attributes.append(name)
+
+    def _am_unlock_attribute(self, name):
+        self._locked_attributes.remove(name)
+
+    def _am_locked_attribute_update(self, name, value):
+        """
+        called when a locked attribute is attempted to be updated
+        """
         pass
