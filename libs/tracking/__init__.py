b4445222a98a38391fc97a5386841847b7c613ea
Eric Davis <6662995+endavis@users.noreply.github.com>
2024-12-21 19:09:55 +0000

    remove tracking module files

--- libs/tracking/__init__.py
+++ libs/tracking/__init__.py
@@ -5,23 +5,13 @@
 # File Description: a "package" to manage records
 #
 # By: Bast
-"""
-This "package"" has classes for various trackable objects
+"""This "package"" has classes for various trackable objects
 
 The main one will be the class AttributeMonitor,
 which will monitor the attributes of an object as well
 as convert lists and dicts to TrackedList and TrackedDict
 """
 
-__all__ = ['AttributeMonitor', 'TrackedDict', 'TrackedList',
-           'TrackedAttr', 'ChangeLogEntry',
-           'add_to_ignore_in_stack']
+__all__ = ["AttributeMonitor"]
 
-from .types._trackbase import TrackBase
-from .types.trackeddict import TrackedDict
-from .types.trackedlist import TrackedList
-from .types.trackedattributes import TrackedAttr
 from .utils.attributes import AttributeMonitor
-from .utils.changelog import ChangeLogEntry
-
-from .utils.changelog import add_to_ignore_in_stack
